5fde9db069e177b9ed70ab674db4dc1c00ea0cd3
BlackCap <mailhph@gmail.com>
2018-10-04 23:42:02 +0200

    Update SnakeMess.cs

--- SnakeMess/SnakeMess.cs
+++ SnakeMess/SnakeMess.cs
@@ -2,53 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
-
-namespace SnakeMess
-{
+namespace SnakeMess {
   class Point {
     public int X { get; set; }
     public int Y { get; set; }
   }
-
-  class SnakeMess
-  {
-    public static void Main()
-    {
+  class SnakeMess {
+    public static void Main() {
       int         newDir = 2;
       bool        pause  = false;
       int         boardW = Console.WindowWidth;
       int         boardH = Console.WindowHeight;
       Random      rng    = new Random();
       Point       app    = new Point();
-      List<Point> snake  = new List<Point>();
+      List<Point> snake  = Enumerable.Repeat<Point>(new Point{ X=10, Y=10 }, 4).ToList();
       Stopwatch   t      = new Stopwatch(); t.Start();
-
-      for (int i=0; i<4; i++) snake.Add(new Point{ X=10, Y=10 });
-
       Console.Clear();
       Console.CursorVisible = false;
       Console.Title = "Høyskolen Kristiania - SNAKE";
       spawn();
-
       while (true) {
         ConsoleKey k = Console.KeyAvailable? Console.ReadKey(true).Key: 0;
-        for (int i = 0; i < 4; i++)
-          if ((int) k == (40 - (i*7)%4) && newDir != i) newDir = (2+i)%4;
-
+        Enumerable.Range(0,4).ToList().ForEach(i=>{if((int)k==(40-(i*7)%4)&&newDir!=i)newDir=(2+i)%4;});
         Point newH = new Point { X=snake.Last().X-((newDir-1)*3-1)%2, Y=snake.Last().Y+(newDir*3-1)%2 };
-
-        if ( k == ConsoleKey.Escape
-          || newH.X < 0 || newH.X >= boardW
-          || newH.Y < 0 || newH.Y >= boardH
-          || hit(newH)
-           ) break;
-
+        if (k==ConsoleKey.Escape||newH.X<0||newH.X>=boardW||newH.Y<0||newH.Y>=boardH||hit(newH))break;
         if ((pause ^= k == ConsoleKey.Spacebar) || t.ElapsedMilliseconds < 100) continue;
         t.Restart();
-
-        if (newH.X == app.X && newH.Y == app.Y)
-          spawn(); else snake.RemoveAt(0);
-
+        if (newH.X == app.X && newH.Y == app.Y) spawn(); else snake.RemoveAt(0);
         put(new[] {snake.Last(),newH,snake.First()}, ConsoleColor.Yellow, "0@ ");
         snake.Add(newH);
       }
